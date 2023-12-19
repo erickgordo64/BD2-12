a04769c8780842a65bd9473e09f31801506884f1
erickgordo64 <erickgordo64@gmail.com>
2023-12-18 21:38:14 -0600

    cambios nuevos

--- practica 2/cli/main.py
+++ practica 2/cli/main.py
@@ -8,10 +8,10 @@ class HospitalCLI:
     def conectar_bd(self, username, password):
         try:
             self.db_connection = mysql.connector.connect(
-                host="tu_host",
+                host="localhost",
                 user=username,
                 password=password,
-                database="tu_base_de_datos"
+                database="practica1"
             )
             return True
         except mysql.connector.Error as err:
@@ -24,9 +24,23 @@ class HospitalCLI:
         password = input("INGRESE CONTRASEÑA: ")
 
         if self.conectar_bd(username, password):
-            print(f"Bienvenido, {username}! Sesión iniciada.")
-            self.logged_in_user = username
-            self.menu_sesion_iniciada()
+            if self.validar_credenciales(username, password):
+                print(f"Bienvenido, {username}! Sesión iniciada.")
+                self.logged_in_user = username
+                self.menu_sesion_iniciada()
+
+    def validar_credenciales(self, username, password):
+        cursor = self.db_connection.cursor()
+
+        # Consulta para verificar las credenciales en la base de datos
+        query = "SELECT * FROM Usuario WHERE username = %s AND password = %s"
+        cursor.execute(query, (username, password))
+        user_data = cursor.fetchone()
+
+        cursor.close()
+
+        return user_data is not None
+
 
     def menu_sesion_iniciada(self):
         while True:
@@ -44,9 +58,9 @@ class HospitalCLI:
             opcion = input("INGRESE UNA OPCION: ")
 
             if opcion == "1":
-                self.actualizar_registros()
-            elif opcion == "2":
                 self.consultas()
+            elif opcion == "2":
+                self.actualizar_registros()
             elif opcion == "3":
                 self.agregar_registros()
             elif opcion == "4":
@@ -67,20 +81,120 @@ class HospitalCLI:
                 print("Opción no válida. Por favor, ingrese un número del 0 al 7.")
 
     def actualizar_registros(self):
-        # Implementa la lógica para actualizar registros
-        pass
+        while True:
+            print(f"***** BIENVENIDO USUARIO: {self.logged_in_user} *****")
+            print("MENU:")
+            print("1. PACIENTES")
+            print("2. HABITACIONES")
+            print("3. LOG ACTIVIDAD")
+            print("4. LOG HABITACION")
+            print("0. CERRAR SESION")
+
+            opcion = input("INGRESE UNA OPCION: ")
+
+            if opcion == "1":
+                self.submenu_pacientes()
+            elif opcion == "2":
+                self.submenu_habitaciones()
+            elif opcion == "3":
+                self.submenu_log_actividad()
+            elif opcion == "4":
+                self.submenu_log_habitacion()
+            elif opcion == "0":
+                print("Cerrando sesión. ¡Hasta luego!")
+                if self.db_connection:
+                    self.db_connection.close()
+                self.logged_in_user = None
+                break
+            else:
+                print("Opción no válida. Por favor, ingrese un número del 0 al 4.")
 
     def consultas(self):
-        # Implementa la lógica para consultas
-        pass
+        while True:
+            print(f"***** BIENVENIDO USUARIO: {self.logged_in_user} *****")
+            print("MENU:")
+            print("1. PACIENTES")
+            print("2. HABITACIONES")
+            print("3. LOG ACTIVIDAD")
+            print("4. LOG HABITACION")
+            print("0. CERRAR SESION")
+
+            opcion = input("INGRESE UNA OPCION: ")
+
+            if opcion == "1":
+                self.submenu_pacientes()
+            elif opcion == "2":
+                self.submenu_habitaciones()
+            elif opcion == "3":
+                self.submenu_log_actividad()
+            elif opcion == "4":
+                self.submenu_log_habitacion()
+            elif opcion == "0":
+                print("Cerrando sesión. ¡Hasta luego!")
+                if self.db_connection:
+                    self.db_connection.close()
+                self.logged_in_user = None
+                break
+            else:
+                print("Opción no válida. Por favor, ingrese un número del 0 al 4.")
 
     def agregar_registros(self):
-        # Implementa la lógica para agregar registros
-        pass
+        while True:
+            print(f"***** BIENVENIDO USUARIO: {self.logged_in_user} *****")
+            print("MENU:")
+            print("1. PACIENTES")
+            print("2. HABITACIONES")
+            print("3. LOG ACTIVIDAD")
+            print("4. LOG HABITACION")
+            print("0. CERRAR SESION")
+
+            opcion = input("INGRESE UNA OPCION: ")
+
+            if opcion == "1":
+                self.submenu_pacientes()
+            elif opcion == "2":
+                self.submenu_habitaciones()
+            elif opcion == "3":
+                self.submenu_log_actividad()
+            elif opcion == "4":
+                self.submenu_log_habitacion()
+            elif opcion == "0":
+                print("Cerrando sesión. ¡Hasta luego!")
+                if self.db_connection:
+                    self.db_connection.close()
+                self.logged_in_user = None
+                break
+            else:
+                print("Opción no válida. Por favor, ingrese un número del 0 al 4.")
 
     def eliminar_registros(self):
-        # Implementa la lógica para eliminar registros
-        pass
+        while True:
+            print(f"***** BIENVENIDO USUARIO: {self.logged_in_user} *****")
+            print("MENU:")
+            print("1. PACIENTES")
+            print("2. HABITACIONES")
+            print("3. LOG ACTIVIDAD")
+            print("4. LOG HABITACION")
+            print("0. CERRAR SESION")
+
+            opcion = input("INGRESE UNA OPCION: ")
+
+            if opcion == "1":
+                self.submenu_pacientes()
+            elif opcion == "2":
+                self.submenu_habitaciones()
+            elif opcion == "3":
+                self.submenu_log_actividad()
+            elif opcion == "4":
+                self.submenu_log_habitacion()
+            elif opcion == "0":
+                print("Cerrando sesión. ¡Hasta luego!")
+                if self.db_connection:
+                    self.db_connection.close()
+                self.logged_in_user = None
+                break
+            else:
+                print("Opción no válida. Por favor, ingrese un número del 0 al 4.")
 
     def realizar_respaldo_completo(self):
         # Implementa la lógica para realizar un respaldo completo
@@ -93,6 +207,46 @@ class HospitalCLI:
     def restaurar_respaldo(self):
         # Implementa la lógica para restaurar un respaldo
         pass
+
+    def submenu_pacientes(self):
+        pass
+
+    def submenu_habitaciones(self):
+        pass
+
+    def submenu_log_actividad(self):
+        pass
+
+    def submenu_log_habitacion(self):
+       pass
+
+    def registrar_usuario(self):
+        print("*** REGISTRO DE NUEVO USUARIO ***")
+        nuevo_usuario = input("INGRESE NUEVO USUARIO: ")
+        nueva_contrasena = input("INGRESE CONTRASEÑA: ")
+        usuario_admin = input("INGRESE USUARIO ADMINISTRADOR: ")
+        contrasena_admin = input("INGRESE CONTRASEÑA ADMINISTRADOR: ")
+        rol_nuevo_usuario = input("INGRESE EL ROL DEL NUEVO USUARIO: ")
+
+        if self.conectar_bd(usuario_admin, contrasena_admin):
+            cursor = self.db_connection.cursor()
+
+            # Verificar si el usuario administrador y la contraseña son correctos
+            query_admin = "SELECT * FROM usuarios WHERE username = %s"
+            cursor.execute(query_admin, (usuario_admin,))
+            admin_data = cursor.fetchone()
+
+            if not admin_data:
+                print("Credenciales del administrador incorrectas. No se puede registrar el nuevo usuario.")
+            else:
+                # Si las credenciales del administrador son correctas, registrar el nuevo usuario
+                insert_query = "INSERT INTO usuarios (username, password, rol) VALUES (%s, %s, %s)"
+                cursor.execute(insert_query, (nuevo_usuario, nueva_contrasena, rol_nuevo_usuario))
+                self.db_connection.commit()
+                print(f"Usuario {nuevo_usuario} registrado exitosamente como {rol_nuevo_usuario}.")
+
+            cursor.close()
+            self.db_connection.close()
 
     def menu_principal(self):
         while True:
