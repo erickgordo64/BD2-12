1f42c78e4ebac8024db0f937f1490e7d0a5df249
erick <erickgordo64@gmail.com>
2023-12-27 21:58:42 -0600

    cambios git ignore

--- proyecto/frontend/proyecto/src/App.js
+++ proyecto/frontend/proyecto/src/App.js
@@ -1,16 +1,13 @@
+import logo from './logo.svg';
 import './App.css';
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from './components/Login/Login';
 import Layout from './components/Layout/Layout';
 import Home from './pages/Home';
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Layout/>} />
-        <Route index element={<Home/>}/>
-    </Routes>
     </BrowserRouter>
   );
 }
